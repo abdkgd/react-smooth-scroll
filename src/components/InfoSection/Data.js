ddd0e99075d0b921789be8eed1b06e489bc310c5
Jeffrey Esma <abd.kgd@gmail.com>
2020-11-24 00:50:53 +0800

    Creating InfoSection and Adding Data

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,25 @@
+import svg1 from '../../images/svg-1.svg';
+// import svg2 from '../../images/svg-1.svg';
+// import svg3 from '../../images/svg-1.svg';
+// import svg4 from '../../images/svg-1.svg';
+// import svg5 from '../../images/svg-1.svg';
+// import svg6 from '../../images/svg-1.svg';
+
+export const homeObjOne = {
+	id: 'about',
+	lightbg: false,
+	lightText: true,
+	lightTextDesc: true,
+	darkText: false,
+	topLine: 'Premium Bank',
+	headline: 'Unlimited Transactions with zero fees',
+	description:
+		'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+	buttonLabel: 'Get Started',
+	imgStart: false,
+	img: svg1,
+	alt: 'Car',
+	dark: true,
+	dark2: true,
+	primary: true,
+};
